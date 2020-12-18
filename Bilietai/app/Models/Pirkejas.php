ced0bc50313a82334af195699dedd45c7e41c63a
ediszz <edvinas.riepsas@gmail.com>
2020-12-18 22:27:34 +0200

    Sukurti modeliu failai

---
+++ Bilietai/app/Models/Pirkejas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pirkejas extends Model
+{
+    use HasFactory;
+}
