487efefaffcd0f1dc0d168b323c9a7448699a0b6
ediszz <edvinas.riepsas@gmail.com>
2020-12-18 22:55:49 +0200

    Aprasytas pirkejo modelis

--- Bilietai/app/Models/Pirkejas.php
+++ Bilietai/app/Models/Pirkejas.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pirkejas extends Model
 {
-    use HasFactory;
+    protected $table = 'pirkejas';
+    protected $fillable = ['Taskai', 'NaujienlaiskioPrenumerata', 'Amzius', 'TelefonoNumeris', 'Lytis', 'fk_Vartotojasid_Vartotojas', 'fk_Apmokejimasid_Apmokejimas'];
+    protected $primaryKey = 'id_Pirkejas';
+    public $timestamps = false;
 }
