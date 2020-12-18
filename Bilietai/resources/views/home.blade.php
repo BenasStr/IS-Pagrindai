5e709548fa6fbf5ab80cc3068bde0a000e6ead38
StBenas <strauka.benas@gmail.com>
2020-12-18 21:44:28 +0200

    Initial laravel project commit

---
+++ Bilietai/resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Bilietu kurimo app</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <!--Header-->
+<nav class="navbar navbar-expand-sm bg-warning navbar-light">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <img src="{{ asset('/images/bee.png') }}" alt="bee" style="width:40px;height:30px;">
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="{{ url('/') }}">BEElietai</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+        </li>
+    </ul>
+</nav>
+
+</body>
+</html>
