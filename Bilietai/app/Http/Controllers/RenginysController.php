9299942e737ea069f590b6ae126a45fbc285cc64
StBenas <strauka.benas@gmail.com>
2020-12-19 16:15:38 +0200

    Created search for events

--- Bilietai/app/Http/Controllers/RenginysController.php
+++ Bilietai/app/Http/Controllers/RenginysController.php
@@ -9,13 +9,27 @@ use App\Models\Renginys;
 class RenginysController extends Controller
 {
     public function getAllEvents() {
-        $allEvents = Renginys::all();
+        $events = Renginys::all();
         $count = Renginys::all()->count();
-        return view('events/events', compact(['allEvents', 'count']));
+        return view('events/events', compact(['events', 'count']));
     }
 
     public function getEvent($id) {
         $event = Renginys::all()->where('id_Renginys', $id)->first();
         return view('events/event', compact('event'));
+    }
+
+    public function getFilteredEvents(Request $request) {
+        $key = "/".$request->input('search')."/i";
+        $allEvents = Renginys::all();
+        $events = array();
+        foreach ($allEvents as $event) {
+            $name = $event->Pavadinimas;
+            if(preg_match($key, $name)){
+                array_push($events, $event);
+            }
+        }
+        $count = count($events);
+        return view('events/events', compact(['events', 'count']));
     }
 }
--- Bilietai/resources/views/events/events.blade.php
+++ Bilietai/resources/views/events/events.blade.php
@@ -1,37 +1,41 @@
 @extends('home')
 
 @section('content')
-    <div class="row" style="margin-top: 10px; margin-bottom: 10px; margin-left: 25px">
-        <form class="form-inline" action="{{ url('/') }}" method="post">
+    <div class="row" style="margin-top: 10px; margin-bottom: 10px; margin-left: 40px">
+        <form class="form-inline" action="{{ url('/search') }}" method="post">
             {{ csrf_field() }}
             <input class="form-control mr-sm-2" type="text" placeholder="Įveskite raktažodį" name="search">
-            <button class="btn btn-success" type="submit">Ieškoti</button>
+            <button type="submit" class="btn btn-success">Ieškoti</button>
         </form>
     </div>
     <table style="margin-left: 40px">
-        <?php $cnt = 0;?>
-        @for($i = 0; $i < $count; $i++)
+        @if($count == 0)
+            <h2>Nepavyko rasti duomenų<h2>
+        @else
             <?php $cnt = 0;?>
-            @if($i%4 == 0)
-                <tr>
-                    @endif
-                    <td>
-                        <div class="card">
-                            <div class="card-header" style="text-align: center; background-color: #fbc02d">{{ $allEvents[$i]->Pavadinimas }}</div>
-                            <div class="card-body">
-                                <div>Miestas: {{ $allEvents[$i]->Miestas }}</div>
-                                <div>Aprašymas: {{ $allEvents[$i]->Aprasymas }}</div>
-                            </div>
-                            <div class="card-footer">
-                                <a class="btn btn-success" href="{{ route('event', $allEvents[$i]->id_Renginys ) }}">Peržiūrėti</a>
-                            </div>
-                        </div>
-                    </td>
-                    <?php $cnt++;?>
-                    @if($cnt == 4)
-                </tr>
+            @for($i = 0; $i < $count; $i++)
                 <?php $cnt = 0;?>
-            @endif
-        @endfor
+                @if($i%4 == 0)
+                    <tr>
+                        @endif
+                        <td>
+                            <div class="card">
+                                <div class="card-header" style="text-align: center; background-color: #fbc02d">{{ $events[$i]->Pavadinimas }}</div>
+                                <div class="card-body">
+                                    <div>Miestas: {{ $events[$i]->Miestas }}</div>
+                                    <div>Aprašymas: {{ $events[$i]->Aprasymas }}</div>
+                                </div>
+                                <div class="card-footer">
+                                    <a class="btn btn-success" href="{{ route('event', $events[$i]->id_Renginys ) }}">Peržiūrėti</a>
+                                </div>
+                            </div>
+                        </td>
+                        <?php $cnt++;?>
+                        @if($cnt == 4)
+                    </tr>
+                    <?php $cnt = 0;?>
+                @endif
+            @endfor
+        @endif
     </table>
 @endsection
