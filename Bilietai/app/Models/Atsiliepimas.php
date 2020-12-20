e06fbd081ddd96343cd8657588bb342cdc91b8fe
StBenas <strauka.benas@gmail.com>
2020-12-20 14:53:14 +0200

    Sutvarkyta atsiliepimu perziura

--- Bilietai/app/Models/Atsiliepimas.php
+++ Bilietai/app/Models/Atsiliepimas.php
@@ -8,12 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Atsiliepimas extends Model
 {
     protected $table = 'atsiliepimas';
-    protected $fillable = ['Ivertinimas', 'Aprasymas', 'SukurimoData', 'fk_Renginysid_Renginys', 'fk_Pardavejasid_Pardavejas'];
+    protected $fillable = ['Ivertinimas', 'Aprasymas', 'SukurimoData', 'fk_Renginysid_Renginys', 'fk_Pirkejasid_Pirkejas', 'fk_Pardavejasid_Pardavejas'];
     protected $primaryKey = 'id_Atsiliepimas';
 
     public $timestamps = false;
 
     public function renginys() {
         return $this->belongsTo(Renginys::class, 'fk_Renginysid_Renginys');
+    }
+
+    public function pirkejas() {
+        return $this->belongsTo(Pirkejas::class, 'fk_Pirkejasid_Pirkejas');
     }
 }
