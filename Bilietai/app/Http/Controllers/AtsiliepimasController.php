698c13e40954c14903e49e5b23ab6acf134e790d
StBenas <strauka.benas@gmail.com>
2020-12-20 20:06:45 +0200

    Feedback fix

--- Bilietai/app/Http/Controllers/AtsiliepimasController.php
+++ Bilietai/app/Http/Controllers/AtsiliepimasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Renginys;
 use Illuminate\Http\Request;
 use App\Models\Atsiliepimas;
 use App\Models\Pirkejas;
